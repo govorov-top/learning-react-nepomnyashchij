0050112541b1759194abed75b6ce4533f2b0e34a
RGameShow <info@roman-web-developer.ru>
2022-12-27 20:29:33 +0300

    Start learn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Preloader from "./Preloader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    render(){
+        return this.props.isLoading ? (<Preloader/>) : (
+            <div className="App">
+                <h1>HI!</h1>
+                <button onClick={this.handleClick}>{this.state.count}</button>
+            </div>
+        ) ;
+    }
 }
 
 export default App;
---
+++ src/Preloader.js
@@ -0,0 +1,9 @@
+function Preloader() {
+  return (
+    <div className="App">
+      <h3>Loading..</h3>
+    </div>
+  );
+}
+
+export default Preloader;
